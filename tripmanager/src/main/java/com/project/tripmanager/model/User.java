0546219eaaf5244aa01a2a14c7ea7deea7fdd4af
Rakesh3555 <rakesh.arunagirisamy@chainsys.com>
2024-07-02 17:20:14 +0530

    Pojo Package Rename

---
+++ tripmanager/src/main/java/com/project/tripmanager/model/User.java
@@ -0,0 +1,57 @@
+package com.project.tripmanager.model;
+
+public class User {
+	
+	String username;
+	String password;
+	String mail;
+	String generaterOtp;
+	String mobileNumber;
+	String valOtp;
+	String tripPackage;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getGeneraterOtp() {
+		return generaterOtp;
+	}
+	public void setGeneraterOtp(String generaterOtp) {
+		this.generaterOtp = generaterOtp;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getValOtp() {
+		return valOtp;
+	}
+	public void setValOtp(String valOtp) {
+		this.valOtp = valOtp;
+	}
+	public String getTripPackage() {
+		return tripPackage;
+	}
+	public void setTripPackage(String tripPackage) {
+		this.tripPackage = tripPackage;
+	}
+	
+	
+
+}
